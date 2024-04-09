65a5a970179b09c2d00e977b313a50206cc2e839
Sneha <Sneha@DESKTOP-2SSD8HG>
2024-04-09 10:14:13 +0530

    COMMIT

---
+++ src/grocery_shopping/Grocery_shopping.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package grocery_shopping;
+
+/**
+ *
+ * @author Sneha
+ */
+public class Grocery_shopping {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        loginpage loginframe = new loginpage();
+        loginframe.setVisible(true);
+        loginframe.pack();
+        loginframe.setLocationRelativeTo(null);
+    }
+    
+}
